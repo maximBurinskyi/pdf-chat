a2bf0fd08e0ca2965635dea2f183ace8053febae
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-11-12 10:02:16 +0200

    finished landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,143 @@ import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Link from 'next/link';
 import { ArrowRight } from 'lucide-react';
 import { buttonVariants } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">
-          Pdf search is public!
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">
+            Pdf search is public!
+          </p>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Chat with your <span className="text-blue-600">documents</span> in
+          seconds
+        </h1>
+        <p className="mt-5 max-w-prose text-zink-700 sm:text-lg">
+          Program allows you to have conversations with any pdf documents.
+          Upload your file and start asking questions
         </p>
+        <Link
+          className={buttonVariants({
+            size: 'lg',
+            className: 'mt-5',
+          })}
+          href="/dashboard"
+          target="_blank"
+        >
+          Get started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWidthWrapper>
+
+      {/* value proposition section */}
+      <div>
+        <div className="relative isolate">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" />
+          </div>
+
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    className="rounded-lg bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                    src="/dashboard-preview.jpg"
+                    width={1364}
+                    height={866}
+                    alt="product preview"
+                    quality={100}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]" />
+          </div>
+        </div>
       </div>
-      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-        Chat with your <span className="text-blue-600">documents</span> in
-        seconds
-      </h1>
-      <p className="mt-5 max-w-prose text-zink-700 sm:text-lg">
-        Program allows you to have conversations with any pdf documents. Upload
-        your file and start asking questions
-      </p>
-      <Link
-        className={buttonVariants({
-          size: 'lg',
-          className: 'mt-5',
-        })}
-        href="/dashboard"
-        target="_blank"
-      >
-        Get started <ArrowRight className="ml-2 h-5 w-5" />
-      </Link>
-    </MaxWidthWrapper>
+      {/* Feature section */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8 ">
+          <div className="mx-auto max-w-2xl sm:text-center ">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start chatting in minutes
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Chatting to your PDF files has never been easier.
+            </p>
+          </div>
+        </div>
+        {/* steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zink-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">STEP 1</span>
+              <span className="text-xl font-semibold">Sign up for account</span>
+              <span className="mt-2 text-zinc-700">
+                Either starting out with free plan or choose our{' '}
+                <Link
+                  href="/pricing"
+                  className="text-blue-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+              </span>
+            </div>
+          </li>
+
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zink-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">STEP 2</span>
+              <span className="text-xl font-semibold">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We&apos;ll process your file and make it ready for your to chat
+                with.
+              </span>
+            </div>
+          </li>
+
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zink-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">STEP 3</span>
+              <span className="text-xl font-semibold">
+                Start asking questions
+              </span>
+              <span className="mt-2 text-zinc-700">
+                It&apos;s that simple. It takes less than a minute.
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image
+                className="rounded-lg bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                src="/file-upload-preview.jpg"
+                width={1419}
+                height={732}
+                alt="uploading preview"
+                quality={100}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
