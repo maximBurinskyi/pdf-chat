227c5c81d50c86e7f728c574a9f425cb389b6dec
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-11-16 05:15:18 +0200

    created pdf renderer functionality

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,8 +1,55 @@
-const PdfRenderer = () => {
+'use client';
+
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import { Loader2, ChevronDown } from 'lucide-react';
+import { useToast } from './ui/use-toast';
+import { useResizeDetector } from 'react-resize-detector';
+import { Button } from './ui/button';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+
+  const { width, ref } = useResizeDetector();
   return (
     <div className="w-full bg-white rounded-md  shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5">top bar</div>
+        <div className="flex items-center gap-1.5">
+          <Button variant="ghost" aria-label="previous page">
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5"></div>
+        </div>
+      </div>
+
+      <div className="flex-1  w-full max-h-screen">
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: 'Error loading PDF',
+                description: 'Please try again later',
+                variant: 'desctructive',
+              });
+            }}
+            file={url}
+            className="max-h-full "
+          >
+            <Page width={width} pageNumber={1} />
+          </Document>
+        </div>
       </div>
     </div>
   );
