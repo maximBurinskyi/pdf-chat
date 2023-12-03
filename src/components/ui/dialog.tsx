d0017a96356903f15923e2ab6c0597f4e20a55aa
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-12-03 06:13:08 +0200

    changed dialog2

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -101,7 +101,11 @@ const DialogDescription = React.forwardRef<
 >(({ ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn('text-sm text-muted-foreground', className)}
+    className={cn(
+      'text-sm text-muted-foreground'
+
+      // className
+    )}
     {...props}
   />
 ));
